913684210128e7bcbb8453bbaa5cda43f6179da9
SouleCode <souleymanetraore.22026@gmail.com>
2024-08-09 21:23:16 +0100

    Le docs

---
+++ blog.com/includes/head.php
@@ -0,0 +1,16 @@
+<header>
+		<nav>
+			<div class="container">
+				<ul>
+					<li><a href="/">ACCUEIL</a></li>
+					<li><a href="/?categorie=cinema">CINEMA</a></li>
+					<li><a href="#">A PROPOS</a></li>
+					<li><a href="#">CONTACT</a></li>
+				</ul>
+			</div>
+		</nav>
+		<h1>BLOG</h1>
+	</header>
+
+	<div class="container main">
+		<section class="content">
